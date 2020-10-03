3eeddafdd95fbde33e43bfe82510eb69030c0556
Daniel Whiffing <danielwhiffing@gmail.com>
2020-10-03 14:55:51 -0400

    Fixes trigger issues

--- src/sprites/Trigger.js
+++ src/sprites/Trigger.js
@@ -1,14 +1,13 @@
 export class Trigger extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, object) {
-    super(scene, object.x + 50, object.y + 25, 'tilemap')
+    super(scene, object.x, object.y)
     this.overlap = this.overlap.bind(this)
     this.object = object
     this.scene.physics.world.enable(this)
     this.scene.add.existing(this)
     this.setOrigin(0)
-    this.setOffset(0)
-    this.setAlpha(0)
-    this.setSize(object.width, object.height)
+    // this.setAlpha(0)
+    this.setSize(object.width, object.height, false)
 
     this.hasTriggered = false
   }
@@ -16,7 +15,8 @@ export class Trigger extends Phaser.Physics.Arcade.Sprite {
   overlap() {
     if (
       this.hasTriggered &&
-      !this.object.properties.some((p) => p.name === 'continuous')
+      (!this.object.properties ||
+        !this.object.properties.some((p) => p.name === 'continuous'))
     )
       return
     this.hasTriggered = true
